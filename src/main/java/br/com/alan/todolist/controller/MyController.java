e83f6ea243640e6c383345cb7b3be99dbf3a89aa
xd-elon <alandevelope@gmail.com>
2024-09-29 00:04:31 -0300

    task

--- src/main/java/br/com/alan/todolist/controller/MyController.java
+++ src/main/java/br/com/alan/todolist/controller/MyController.java
@@ -1,6 +1,14 @@
 package br.com.alan.todolist.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/task")
 public class MyController {
-  
+
+  public String primeiraMensagem() {
+    return "ok";
+  }
+
 }
